722539ee40fba11096349e233eabe883df0c7993
Dean Fox <deanfoxcd@gmail.com>
2024-11-09 20:29:43 -0500

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
 
 function App() {
   return (
     <div className="card">
-      <Avatar />
+      <Avatar src="avatar.jpeg" />
       <div className="data">
-        <Intro />
-        {/* Should contain one Skill component
-        for each web dev skill that you have,
-        customized with props */}
+        <Intro
+          name="Dean Fox"
+          intro="Learning full-stack development in order to change careers. Have a massive headache right now and can't think of anything interesting to write"
+        />
         <SkillList />
       </div>
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const Avatar = (props) => {
+  return <img src={props.src} alt="dean" className="avatar"></img>;
+};
+
+const Intro = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p className="data">{props.intro}</p>
+    </div>
+  );
+};
+
+const SkillList = (props) => {
+  return (
+    <div className="skill-list">
+      <Skill skill="JavaScript" color="red" />
+      <Skill skill="HTML" color="orange" />
+      <Skill skill="Python" color="green" />
+      <Skill skill="React" color="blue" />
+    </div>
+  );
+};
+
+const Skill = (props) => {
+  return (
+    <span className="skill" style={{ backgroundColor: props.color }}>
+      {props.skill} 💪🏻
+    </span>
+  );
+};
+
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
